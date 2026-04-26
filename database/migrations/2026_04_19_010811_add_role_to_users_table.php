caf8d428f4e8cda55762185bd52db09d61e4cce8
Rizki Setiawan <rzkstwn@gmail.com>
2026-04-26 20:32:45 +0700

    fix role migration

--- database/migrations/2026_04_19_010811_add_role_to_users_table.php
+++ database/migrations/2026_04_19_010811_add_role_to_users_table.php
@@ -9,11 +9,13 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up()
+public function up()
 {
-    Schema::table('users', function (Blueprint $table) {
-        $table->string('role')->default('petugas');
-    });
+    if (!Schema::hasColumn('users', 'role')) {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('role')->default('petugas');
+        });
+    }
 }
 
     /**
